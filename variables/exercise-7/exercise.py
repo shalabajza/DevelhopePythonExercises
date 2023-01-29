c82fb4b4f3482d8954077b221b9de99709db246a
Danilo Marinkovic <danilomarinkovic.97@gmail.com>
2023-01-29 18:01:40 +0100

    completed code for exercise 7

--- variables/exercise-7/exercise.py
+++ variables/exercise-7/exercise.py
@@ -1,5 +1,23 @@
+"""
+# Original code: 
+
 "Hi,
 my name is
 John Doe"
 
 "python'
+
+"""
+# New code
+
+"Hi, my name is John Doe"
+
+'python'
+
+# Another way to show the code, I wasn't sure if you wanted me to keep the structural integrity or not
+
+"Hi,"
+"my name is"
+"John Doe"
+
+'python'
