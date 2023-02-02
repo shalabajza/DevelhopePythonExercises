f8f6daea9f923299c5dfedd8d8f15e65b0d4d748
Danilo Marinkovic <danilomarinkovic.97@gmail.com>
2023-02-03 00:21:34 +0100

    exercise 3, methods

--- methods/exercise-03/exercise.py
+++ methods/exercise-03/exercise.py
@@ -6,3 +6,19 @@ print("third element: ", num1_str[2])
 print("elements 3-5: ", num1_str[2:5])
 
 # check if num2, num3 in string? 
+# decided to use placeholder values as a proof of concept
+
+num2 = 112
+num3 = 45567
+
+print(str(num2) in num1_str)
+print(str(num3) in num1_str)
+
+string_with_0 = '0' + num1_str
+print(string_with_0[0:4])
+print(string_with_0[5:])
+
+#### use negative indexing to reach the "567" string_with_0
+# There is no '567' part in the string. I'll just add that to the end if that's what you wanted to do
+string_with_0 += "567"
+print(string_with_0[-3:])
