29d7e1c61a46d789feb82ea95ffc28d8bceeae39
Danilo Marinković <danilomarinkovic.97@gmail.com>
2023-03-02 01:36:42 +0100

    classes exercise 3

--- classes/exercise-03/exercise.py
+++ classes/exercise-03/exercise.py
@@ -1,19 +1,19 @@
-
 class Animal:
-    def __init__(self,legs_count):
+    def __init__(self, legs):
         print("Animal object was created")
-        self.number_of_legs = legs_count
+        self._legs = legs
+        self.runs = False
 
-    def runs(self):
-        print("Running started")
+    def isRunning(self):
+        self.runs = True
+        print(f"Running started on {self._legs} legs")
 
     def count_legs(self):
-        print(f"It has {self.number_of_legs}")
+        print(f"It has {self._legs}")
 
     def return_legs(self):
-        return self.number_of_legs
-        
-animal= Animal(4)
-animal.count_legs()
-print(animal.return_legs())
-print(animal.number_of_legs)
+        return self._legs
+
+cat = Animal(4)
+cat.isRunning()
+print(cat._legs)
